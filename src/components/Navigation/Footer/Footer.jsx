d1958c37f0a45429480d50be959583290dfb9b2f
Ice Maya <echaymaya@gmail.com>
2019-02-28 16:52:58 +1300

    added content on footer body/ modified layout for footer nav and media icons

--- src/components/Navigation/Footer/Footer.jsx
+++ src/components/Navigation/Footer/Footer.jsx
@@ -5,8 +5,13 @@ import SocialMediaIcons from '../../Layout/SocialMediaIcons/SocialMediaIcons';
 
 const Footer = (footer) => (
   <React.Fragment>
-    <NavigationItems className={classes.Footer}/>
-    <SocialMediaIcons />
+    <div className={`${classes.Footer} footer`}>
+      <div style={{padding: '70px 0 70px 0'}}>
+        <h2>I would love to work with you. Please get in touch.</h2>
+      </div>
+      <NavigationItems />
+      <SocialMediaIcons />
+    </div>  
   </React.Fragment>
 )
 
