a9c81dfdb33e48ae811c74ab3ecabfaecd92a2ce
Ice Maya <echaymaya@gmail.com>
2019-02-28 17:41:10 +1300

    removed redundant code

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import classes from './NavigationItems.css'
-import LandingPage from '../../Layout/LandingPage/LandingPage'
+import IntroPage from '../../Layout/IntroPage/IntroPage'
 import Experience from '../../Experience/Experience'
 import AboutMe from '../../AboutMe/AboutMe';
 
 const NavigationItems = () => (
     <ul className={`${classes.NavigationItems} navFooter`}>
-        <Link component={LandingPage} to={"/"}>Home</Link>     
+        <Link component={IntroPage} to={"/"}>Home</Link>     
         <Link component={Experience} to="/experience">Experience</Link>
-        <Link component={AboutMe} to="/aboutme">About Me</Link>
+        <Link component={AboutMe} to="/aboutme">About</Link>
         <Link component={Experience} to="/portfolio">Portfolio</Link>
     </ul>
 );
