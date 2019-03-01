9610ebf831554acbf093b3adbc1da3fc98089c58
Ice Maya <echaymaya@gmail.com>
2019-03-02 12:54:48 +1300

    added Sidedrawer feature and to only be visible on mobile view

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './DrawerToggle.css';
+
+const drawerToggle = (props) => (
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <i className="fas fa-bars fa-2x"></i>
+    </div>
+);
+
+export default drawerToggle;
