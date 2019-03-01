e0eb69766f2dd1c96840145f35ed60b3ef81f180
Ice Maya <echaymaya@gmail.com>
2019-03-02 12:54:06 +1300

    added Sidedrawer feature and to only be visible on mobile view

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import Experience from './components/Experience/Experience'
 import Project from './components/Projects'
 import Footer from './components/Navigation/Footer';
 import MainPage from './components/Layout/MainPage';
+import Toolbar from '../src/components/Navigation/Toolbar/Toolbar';
+import SideDrawer from './components/Navigation/SideDrawer/SideDrawer';
+
 
 class App extends Component {
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerClosedHandler = () => {
+      this.setState( { showSideDrawer: false } );
+  }
+
+  sideDrawerToggleHandler = () => {
+      this.setState( ( prevState ) => {
+          return { showSideDrawer: !prevState.showSideDrawer };
+      } );
+  }
+
   render () {
     return (
       <div className='app'>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+            open={this.state.showSideDrawer}
+            closed={this.sideDrawerClosedHandler} />
         <MainPage />
         <AboutMe />
         <Project />
