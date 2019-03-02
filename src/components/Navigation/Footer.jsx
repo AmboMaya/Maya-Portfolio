341afdf10461e64ad68b978dfd2ca5a5dfd4fe16
Ice Maya <echaymaya@gmail.com>
2019-03-02 15:18:33 +1300

    corrected NavigationItems path

--- src/components/Navigation/Footer.jsx
+++ src/components/Navigation/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavigationItems from './NavigationItems';
+import NavigationItems from './NavigationItems/NavigationItems';
 import SocialMediaIcons from '../Layout/SocialMediaIcons';
 
 const Footer = () => (
