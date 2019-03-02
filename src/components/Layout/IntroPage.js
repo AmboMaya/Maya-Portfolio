066e0ccee04b4f3f30403acf1704e527a3113a52
Ice Maya <echaymaya@gmail.com>
2019-03-02 15:15:29 +1300

    added image src to component

--- src/components/Layout/IntroPage.js
+++ src/components/Layout/IntroPage.js
@@ -3,9 +3,10 @@ import React from 'react'
 const IntroPage = () => {
 
   return (
-    <div className='introPage'>
+    <div className='introLine'>
       <h1>Nice to meet you</h1>
       <p>I'm a junior software dev who’s recently made the switch from a non-technical career. I love building digital products and learning cool stuff about technology. </p>
+      <img className='profileImg' src='http://localhost:3000/images/mayaProfileImg.jpg' alt='Get to know me' />
     </div>
   )
 }
