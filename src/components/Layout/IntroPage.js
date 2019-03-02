d9afcffa93ab0eed1ef3ee8ed489fcc71488bbef
Ice Maya <echaymaya@gmail.com>
2019-03-03 00:43:09 +1300

    fixed bug on mobile view as it wasn't showing at 100% width

--- src/components/Layout/IntroPage.js
+++ src/components/Layout/IntroPage.js
@@ -6,7 +6,7 @@ const IntroPage = () => {
     <div className='introLine'>
       <h1>Nice to meet you</h1>
       <p>I'm a junior software dev who’s recently made the switch from a non-technical career. I love building digital products and learning cool stuff about technology. </p>
-      <img className='profileImg' src='http://localhost:3000/images/mayaProfileImg.jpg' alt='Get to know me' />
+      <img className='profileImg' src='/images/mayaProfileImg.jpg' alt='Get to know me' />
     </div>
   )
 }
