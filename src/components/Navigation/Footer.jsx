c28631619504ea9aa60a4e62b1bf7fef17c18c8a
Ice Maya <echaymaya@gmail.com>
2019-03-06 03:24:28 +1300

    refactor: tidied up spacing

--- src/components/Navigation/Footer.jsx
+++ src/components/Navigation/Footer.jsx
@@ -3,14 +3,14 @@ import NavigationItems from './NavigationItems/NavigationItems'
 import SocialMediaIcons from '../Layout/SocialMediaIcons'
 
 const Footer = () => (
-  <div className='footerContainer'>
+	<div className='footerContainer'>
 		<div className='actionParagraph'>
-    		<h2>I would love to work with you. Please get in touch.</h2>
+			<h2>I would love to work with you. Please get in touch.</h2>
     	</div>
     	<NavigationItems />
     	<SocialMediaIcons />
 		<p style={{paddingTop: 10, opacity: 0.3}}>&copy; 2019 AmboMaya. All Rights Reserved.</p>
-  </div>  
+  	</div>  
 )
 
 export default Footer
