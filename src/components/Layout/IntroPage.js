8b028954964fc7ae1fb1ffb2ac488e27fcea3144
Ice Maya <echaymaya@gmail.com>
2019-03-07 15:22:16 +1300

    added more line to intro para

--- src/components/Layout/IntroPage.js
+++ src/components/Layout/IntroPage.js
@@ -8,7 +8,7 @@ const IntroPage = () => {
       <Element name='target1'>
         <h1>Nice to meet you</h1>              
       </Element>      
-      <p>I'm a junior software dev who’s recently made the switch from a non-technical career. I love building digital products and learning cool stuff about technology. </p>
+      <p>I'm a junior software dev who’s recently made the switch from a non-technical career. I love building digital products and learning cool stuff about technology. I would love to work with you. <br /><a className='callToAction'href="mailto:helloambomaya@gmail.com">Please get in touch.</a></p>
       <img className='profileImg' src='/images/mayaProfileImg.jpg' alt='Get to know me' />
     </div>
   )
