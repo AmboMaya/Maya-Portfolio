07f4b6580c8909253e1d52d969dcae9153b92967
Ice Maya <echaymaya@gmail.com>
2019-03-13 11:06:46 +1300

    added class for side drawer navigation items

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,12 +3,12 @@ import { Link } from 'react-scroll'
 import classes from '../NavigationItems/NavigationItems.css'
 
 const NavigationItems = (props) => (
-    <ul className={`${classes.NavigationItems} navigationItems`}>
+    <ul className={`${classes.NavigationItems} navigationItems sideDrawerNav`}>
         <Link to='target1' smooth={true} duration={700} offset={-200}>Home</Link>
         <Link to='target2' smooth={true} duration={700} offset={0}>Experience</Link>
         <Link to='target3' smooth={true} duration={700} offset={-280}>About</Link>
         <Link to='target4' smooth={true} duration={700} offset={-10}>Portfolio</Link>
-    </ul> 
+    </ul>
 )
 
 export default NavigationItems
