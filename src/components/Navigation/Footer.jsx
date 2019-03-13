1a2b55d6c23ac35953425272c5a401c0b820cea2
Ice Maya <echaymaya@gmail.com>
2019-03-13 18:25:15 +1300

    removed action paragraph on footer component

--- src/components/Navigation/Footer.jsx
+++ src/components/Navigation/Footer.jsx
@@ -4,7 +4,6 @@ import SocialMediaIcons from '../Layout/SocialMediaIcons'
 
 const Footer = () => (
 	<div className='footerContainer'>
-		{/* <h2 className='actionParagraph'>I would love to work with you. <br/> Please get in touch.</h2> */}
     	<NavigationItems />
     	<SocialMediaIcons />
 		<p style={{fontSize: 10, 	paddingTop: 10, opacity: 0.3}}>&copy; 2019 AmboMaya. All Rights Reserved.</p>
