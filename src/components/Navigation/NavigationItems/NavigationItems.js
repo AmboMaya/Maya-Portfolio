989e3005b37e836ba9c61d391b79733fd36d4d8a
Ice Maya <echaymaya@gmail.com>
2019-03-13 18:24:12 +1300

    removed unnecessary class on the navigation component. edit styling for responsive mobile view

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,7 +3,7 @@ import { Link } from 'react-scroll'
 import classes from '../NavigationItems/NavigationItems.css'
 
 const NavigationItems = (props) => (
-    <ul className={`${classes.NavigationItems} navigationItems sideDrawerNav`}>
+    <ul className={`${classes.NavigationItems} sideDrawerNav`}>
         <Link to='target1' smooth={true} duration={700} offset={-200}>Home</Link>
         <Link to='target2' smooth={true} duration={700} offset={0}>Experience</Link>
         <Link to='target3' smooth={true} duration={700} offset={-280}>About</Link>
