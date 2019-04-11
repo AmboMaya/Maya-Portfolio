993a3aeff4da5b6543bf38657b4899040eb9c9d8
Ice Maya <echaymaya@gmail.com>
2019-04-11 18:08:50 +1200

    attached new images from project background img

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,8 +5,10 @@ const AboutMe = () => (
 	<div className='aboutMe'>
 		<h1>ABOUT ME</h1>
 		<Element name='target3'>
-			<p>I'm Maya. I love to play guitar and sing, and I have a cat.
-				I love building digital products and creating intuitive websites with a focus on aesthetic offering a great user experience. Last year I decided to take the plunge by quitting my steady job and enrolled in the Enspiral Dev Academy. It's an 18 week, 60+ hours per week web development bootcamp with intense learning to become a full-stack web developer.</p>
+			<p>I'm Maya. In my spare time I play guitar and sing, and play with my cat Kira. I have recently discovered my interest in gardening too. 
+				Eventhough I came from a non-technical background I've always enjoyed being technical and be a problem solver. I love building digital products and creating intuitive websites with a focus on aesthetic offering a great user experience.
+				Last year I decided to take the plunge by quitting my steady job and enrolled in the Enspiral Dev Academy. It's an 18 week, 60+ hours per week web development bootcamp with intense learning to become a full-stack web developer.
+				</p>
 		</Element>
 	</div>
 )
