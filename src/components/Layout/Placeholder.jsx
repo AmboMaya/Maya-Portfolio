c936fdfc9037ce1dea01a21be92830ca444504cc
Ice Maya <echaymaya@gmail.com>
2019-04-11 18:08:18 +1200

    added on hover feature o projects component

--- src/components/Layout/Placeholder.jsx
+++ src/components/Layout/Placeholder.jsx
@@ -3,12 +3,17 @@ import React from 'react'
 const Placeholder = (props) => {
 	return (
 		<div className='projectContainer'>
-			<div className='cardContainer'>
-				<a href={props.url}>
+			<div className="cardContainer overlay-bg">
+				<div className="normal">
 					<h3>{props.title}</h3>
 					<img src={props.image} alt="project I've worked" />
 					<p>{props.description}</p>
-				</a>
+				</div>
+				<div className="hover">
+					<div className="text">
+						<a href={props.url}>See more</a>
+					</div>
+				</div>
 			</div>  
 		</div>
 	)
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,22 +4,34 @@ import { Element } from 'react-scroll'
 
 const projects = [
 	{
-	title: 'Fuelio',
-	url:'https://github.com/AmboMaya/FueLio',
-	image: '/images/fuelio.png',
-	projectDescription: "This is a personal project during EDA, inspired by spiking cost of fuel around Auckland."
+		title: 'Fuelio',
+		url:'https://github.com/AmboMaya/FueLio',
+		image: '/images/fuelio.png',
+		projectDescription: "This is a personal project during EDA, inspired by spiking cost of fuel around Auckland."
 	},
 	{
-	title: 'Mood',
-	url: 'https://kowhai-fp.herokuapp.com/',
-	image: '/images/mood2.png',
-	projectDescription: "Mood records one's daily emotional state and reflect it in graphical data. This is a final team project at EDA."
+		title: 'Mood',
+		url: 'https://kowhai-fp.herokuapp.com/',
+		image: '/images/mood2.png',
+		projectDescription: "Mood records one's daily emotional state and reflect it in graphical data. This is a final team project at EDA."
 	},
 	{
-	title: 'Nightlife',
-	url: 'https://ambomaya.github.io/Art-Project-City-At-Night/js-art-project.html',
-	image: '/images/artProject.png',
-	projectDescription: "A simple art project to demonstrate my practical vanilla JS and CSS animation skills."
+		title: 'Nightlife',
+		url: 'https://ambomaya.github.io/Art-Project-City-At-Night/js-art-project.html',
+		image: '/images/artProject.png',
+		projectDescription: "A simple art project to demonstrate my practical vanilla JS and CSS animation skills."
+	},
+	{
+		title: 'Weather',
+		url: 'https://github.com/AmboMaya/weather-app-prototype',
+		image: '/images/weather.jpg',
+		projectDescription: "This is personal project for a weather app prototype and is written in typescript using react framework."
+	},
+	{
+		title: 'ambomaya.com',
+		url: 'http://ambomaya.com/',
+		image: '/images/ambomaya.jpg',
+		projectDescription: "This portfolio is built with react.js and hosted in AWS services. I designed the site and followed"
 	}
 ]
 
